7535598900df3ddb112be0f8ece7e85b0498885e
yeoreum <jeondaegam@naver.com>
2024-10-11 00:18:16 +0900

    feat: Enemy 행동 패턴 수정
    - As-is: 시간에 따라 상태 변화
    - To-be: 특정 조건에 따라 상태 변화
    - Idle과 Walk에서 거리를 구하는 서로 다른 방법 기억하기

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,7 +13,9 @@ public class Enemy : MonoBehaviour
     enum State
     {
         Idle,
-        Walk
+        Walk,
+        Attack,
+        Dying
     }
 
     State state;
@@ -30,44 +32,85 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
-        // 애니메이션 분리 Idle <-> Walk
+        // 시간에 의지해 상태가 변하는게 아니라
+        // 특정 조건에 따라 상태가 변하도록 수정하자 (많이 사용하는 방식) 
 
-        //1. 걷는 상태인지 쉬는 상태인지 체크
-        //2. 걸을 땐 > SetTrigger " Walk"
-        // 3. 쉴 땐 > SetTrigger "Idle"
-        // 4. 두 애니메이션 사이에 텀을 추가하기
-        // 시간을 셀 변수 필요
+        // 1. 상태 추가 : Dying, Attack
+        // 2. 휴식중일때
+        //플레이어와의 거리를 구한다
+        //    거리가 1m이내이면 공격
+        //    im 밖이면 휴식시간이 지나기를 기다리며 시간을 잰다
+        //    만약 시간이 다되면 걷는다
 
-        timeForNextState -= Time.deltaTime;
-        if (timeForNextState <= 0)
+        //2. 걷는중일때
+        //플레이어에게 도달했거나, 가는 길이 없으면 휴식한다 // TODO 도달하면 Attack 아닌가 ?
+        // 플레이어에게 도착하면 일단 멈추고 -> 휴식한뒤 공격하는구나 
+
+        // 3. 공격중일때
+        // 일정 시간이 지나면 Idle로 돌아간다 
+
+
+        switch (state)
         {
-            switch (state)
-            {
-                case State.Idle:
-                    state = State.Walk;
-                    animator.SetTrigger("Walk");
-                    // 쉬는 시간과 이동 시간을 매번 다르게 설정한다 .
-                    // 동일할 경우 적 캐릭터가 모두 일정한 간격으로 쉬게 되겠지 ? 
-                    timeForNextState = Random.Range(5f, 7f);
-                    agent.isStopped = false;
-                    break;
+            case State.Idle:
+                // 적과 나의 실시간 위치를 기준으로 계산 
+                float distance = (player.position -
+                    (transform.position + GetComponent<CapsuleCollider>().center)).magnitude; // magnitude를 붙이면 Vector의 길이를 float으로 받아온다 . 
+                if (distance < 1.0f)
+                {
+                    Attack();
+                } else
+                {
+                    timeForNextState -= Time.deltaTime;
+                    if (timeForNextState < 0)
+                    {
+                         StartWalk();
+                    }
+                }
+                break;
+            case State.Walk:
+                // agent.remainingDistance: 목표지점과의 남은 거리
+                // (목표지점: 지금은 플레이어가 다른곳으로 이동했을 수도 있기때문에 내가 없을수도 잇음) 
+                if (agent.remainingDistance < 1.0f || !agent.hasPath)
+                {
+                    StartIdle();
+                }
+                break;
+            case State.Attack:
+                timeForNextState -= Time.deltaTime;
+                if (timeForNextState < 0)
+                {
+                    StartIdle();
+                }
+                break;
 
-                case State.Walk:
-                    state = State.Idle;
-                    animator.SetTrigger("Idle");
-                    timeForNextState = Random.Range(1f, 2f);
-                    // TODO Idle 일때도 쉬는 상태로 따라오는데 ?
-                    agent.isStopped = true;
-                    break;
-
-                default:
-                    state = State.Idle;
-                    animator.SetTrigger("Idle");
-                    break;
-
-            }
         }
 
+
+
+    }
+
+    private void StartIdle()
+    {
+        state = State.Idle;
+        timeForNextState = Random.Range(1f, 2f);
+        agent.isStopped = true;
+        animator.SetTrigger("Idle");
+    }
+
+    private void StartWalk()
+    {
+        state = State.Walk;
+        // 목표 지점을 설정한다 . 
+        agent.destination = player.position;
+        agent.isStopped = false;
+        animator.SetTrigger("Walk");
+    }
+
+    private void Attack()
+    {
+        state = State.Attack;
+        timeForNextState = 1.5f;
+        animator.SetTrigger("Attack");
     }
 }
