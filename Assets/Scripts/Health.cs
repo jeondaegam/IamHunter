3fa842393f23e7eb209801c519a574fe3c11783b
yeoreum <jeondaegam@naver.com>
2024-10-11 17:11:08 +0900

    feat: 무적 시간 추가
    - 데미지를 입고 체력이 깎였을 때 0.5초의 무적시간을 둔다
    - 적에게 연속으로 닿아 한번에 여러번 체력이 깎이는 것을 방지하기 위해

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,6 +7,10 @@ public class Health : MonoBehaviour
 {
     public float hp;
     public IHealthListener healthListener;
+    // 무적시간 추가
+    // (연속으로 닿아 한번에 두대 맞는 것을 방지하기 위해 무적 시간을 둔다) 
+    float invincibleTime = 0.5f;
+    float lastAttacTime;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,13 @@ public class Health : MonoBehaviour
 
     public void Damage(float damage)
     {
-        if (hp > 0)
+        // 마지막으로 맞은 시간으로부터 쿨타임이 지난 뒤면 데미지를 입는다. 
+        if (hp > 0 && (lastAttacTime + invincibleTime < Time.time))
         {
             hp -= damage;
+            lastAttacTime = Time.time;
             Debug.Log($"hp:{hp}, damage:{damage}");
+
             if (hp <= 0)
             {
                 if (healthListener != null)
